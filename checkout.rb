ab77430d77ac21cc7a407efd8b4efb98cb52877e
Timothy Lo <tim7643@gmail.com>
2017-07-27 16:40:20 -0500

    give me the total

--- checkout.rb
+++ checkout.rb
@@ -1,5 +1,6 @@
 class Checkout
   attr_accessor :basket
+  @@prices = Hash["CH1" => 3.11, "AP1" => 6.00, "CF1" => 11.23, "MK1" => 4.75]
 
   def initialize
     self.basket = []
@@ -7,6 +8,12 @@ class Checkout
 
   def scan(item)
     @basket.push(item)
+  end
+
+  def total
+    @total = 0
+    @basket.each {|x| @total += @@prices[x]}
+    @total
   end
 
 end
--- checkout_test.rb
+++ checkout_test.rb
@@ -3,15 +3,21 @@ require_relative "checkout"
 
 class CheckoutTest < Minitest::Test
 
-  def test_startup_of_register
+  def setup
     @co = Checkout.new
+  end
+
+  def test_startup_of_register
     assert_equal [], @co.basket
   end
 
   def test_scanning_of_a_chai
-    @co = Checkout.new
     @co.scan("CH1")
     assert_equal ["CH1"], @co.basket
   end
 
+  def test_total_of_a_chai
+    @co.scan("CH1")
+    assert_equal 3.11, @co.total
+  end
 end
